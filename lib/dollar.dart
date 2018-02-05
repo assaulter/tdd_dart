3990f74400edcf8883d0f93cad54e6529ce08da9
Kazuki Kubo <kz@assaulter.org>
2018-02-05 23:51:15 +0900

    fin sec8

--- lib/dollar.dart
+++ lib/dollar.dart
@@ -3,7 +3,7 @@ import 'package:tdd_dart/money.dart';
 class Dollar extends Money {
   Dollar(num amount) : super(amount);
 
-  Dollar times(num multiplier) {
+  Money times(num multiplier) {
     return new Dollar(amount * multiplier);
   }
 }
--- lib/money.dart
+++ lib/money.dart
@@ -1,9 +1,18 @@
-class Money {
+import 'package:tdd_dart/dollar.dart';
+import 'package:tdd_dart/franc.dart';
+
+abstract class Money {
   num amount;
 
   Money(this.amount);
 
+  times(num multiplier);
+
   bool operator == (Object object) {
     return (object is Money) && (this.runtimeType == object.runtimeType) && (amount == object.amount);
   }
+
+  static Money dollar(amount) => new Dollar(amount);
+
+  static Franc franc(amount) => new Franc(amount);
 }
